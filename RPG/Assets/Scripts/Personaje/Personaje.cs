9bbbdd9ab707bbd1f29666eb025eeda87135d8e3
CesarVitalSegura <106455777+CesarVitalSegura@users.noreply.github.com>
2022-11-10 19:34:07 -0600

    animacion_personaje

---
+++ RPG/Assets/Scripts/Personaje/Personaje.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+    {
+    public PersonajeVida PersonajeVida { get;private set; }
+    public PersonajeAnimaciones PersonajeAnimaciones { get; private set; }
+    
+
+    private void Awake()
+    {
+        PersonajeVida = GetComponent<PersonajeVida>();
+        PersonajeAnimaciones = GetComponent<PersonajeAnimaciones>();
+    }
+
+    public void RestaurarPersonaje()
+    {
+        PersonajeVida.RestaurarPersonaje();
+        PersonajeAnimaciones.RevivirPersonaje();
+    }
+
+}
+
